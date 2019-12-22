8ccc51b8fc65e53798f9a812778eb67e312461be
NDS <dddddd1997@gmail.com>
2019-12-22 14:52:42 +0300

    fix change http to https

--- web/index.php
+++ web/index.php
@@ -25,12 +25,23 @@ $app->post('/bot', function() use($app) {
 	if( !$data)
 		return 'nioh';
 
-	if( $data->secret !== getenv('VK_SECRET_TOKEN') && $data->type !== 'confirmation')
+	if( $data->secret !== getenv('VK_SECRET_TOKEN'))
 		return 'nioh';
 
 	switch ($data->type) {
 		case 'confirmation':
 			return getenv('VK_CONFIRM_TOKEN');
+			break;
+		case 'message_new':
+			$request_params = array(
+				'user_id' => $data->object->user_id,
+				'message' => 'Test',
+				'access_token' => getenv(VK_TOKEN),
+				'v' => '5.69'
+			);
+
+			file_get_contents('https://api.vk.com/method/messages.send?'. http_build_query($request_params));
+			return 'ok';
 			break;
 		
 		default:
