775a36b018b5f7503c970ded0d5a1f7d786d769d
NDS <dddddd1997@gmail.com>
2019-12-22 15:14:58 +0300

    add logging

--- web/index.php
+++ web/index.php
@@ -27,19 +27,20 @@ $app->post('/bot', function() use($app) {
 
 	if( $data->secret !== getenv('VK_SECRET_TOKEN'))
 		return 'nioh';
-
+	error_log("in switch");
 	switch ($data->type) {
 		case 'confirmation':
 			return getenv('VK_CONFIRM_TOKEN');
 			break;
 		case 'message_new':
+	error_log("in message_new");
 			$request_params = array(
 				'user_id' => $data->object->user_id,
 				'message' => 'Test',
 				'access_token' => getenv(VK_TOKEN),
 				'v' => '5.69'
 			);
-
+			error_log(request_params)
 			file_get_contents('https://api.vk.com/method/messages.send?'. http_build_query($request_params));
 			return 'ok';
 			break;
