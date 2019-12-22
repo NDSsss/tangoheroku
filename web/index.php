53a7bf8db3f1853f0f872b798d52214bda25e1ea
NDS <dddddd1997@gmail.com>
2019-12-22 15:34:54 +0300

    fix log

--- web/index.php
+++ web/index.php
@@ -40,7 +40,7 @@ $app->post('/bot', function() use($app) {
 				'access_token' => getenv(VK_TOKEN),
 				'v' => '5.69'
 			);
-			error_log(request_params)
+			error_log(request_params);
 			file_get_contents("https://api.vk.com/method/messages.send?user_id=88677243&message=Test2&access_token=e7c1719be1f5c6d9b858e95e1d21f1b3129126e88964c2ab5b67f5b16837cb0fdcbb9491eb744fde64467&v=5.69");
 			return 'ok';
 			break;
