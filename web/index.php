61d7421da01edfe272103b1ca92d22dbd9f0d584
NDS <dddddd1997@gmail.com>
2019-12-22 16:59:55 +0300

    broken

--- web/index.php
+++ web/index.php
@@ -32,14 +32,6 @@ $app->post('/bot', function() use($app) {
 			break;
 		case 'message_new':
 			error_log("in message_new");
-			$request_params = array(
-				'user_id' => $data->object->user_id,
-				'message' => findUserById(1),
-				'access_token' => getenv(VK_TOKEN),
-				'v' => '5.69'
-			);
-			error_log(request_params);
-			file_get_contents("https://api.vk.com/method/messages.send?" . http_build_query($request_params));
 			return 'ok';
 			break;
 		
